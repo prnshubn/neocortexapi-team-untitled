2b23898dcf6fc4d07757e5b4bf280dccf7f427b1
Priyanshu Bandyopadhyay <priyanshu.banerjee1311@gmail.com>
2025-01-18 20:48:24 +0100

    added the required experiment file

---
+++ source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
@@ -0,0 +1,8 @@
+namespace NeoCortexApi.Experiments.SpatialPoolerInputReconstruction
+{
+    // TODO: Input -> Scaler Encoder -> Spatial Pooler -> Classifier -> Output
+    public class SpatialPoolerInputReconstructionExperiment
+    {
+        
+    }
+}
