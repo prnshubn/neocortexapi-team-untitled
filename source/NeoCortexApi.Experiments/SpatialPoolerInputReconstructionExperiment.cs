ebd92a60b3b9639b2ba2fc4429e7207ce409569a
Akshay-Gudekar <akshaygudekar01@gmail.com>
2025-01-19 19:46:42 +0100

    added scalar inputs

--- source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
+++ source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
@@ -1,8 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
 namespace NeoCortexApi.Experiments.SpatialPoolerInputReconstruction
 {
-    // TODO: Input -> Scaler Encoder -> Spatial Pooler -> Classifier -> Output
+    /// <summary>
+    /// Input -> Scalar Encoder -> Spatial Pooler -> Classifier -> Output
+    /// </summary>
+    [TestClass]
     public class SpatialPoolerInputReconstructionExperiment
     {
-        
+        [TestMethod]
+        [TestCategory("Experiment")]
+        public void Setup()
+        {
+            int[] scalarInputs = { 1, 2, 3, 4, 5 };
+        }
+
+
     }
 }
