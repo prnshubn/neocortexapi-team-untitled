841543fdc01d5d3060d7fab929cf95bc2c6169e8
Priyanshu Bandyopadhyay <priyanshu.banerjee1311@gmail.com>
2025-01-19 21:08:33 +0100

    changed the codebase by incorporating previous implementation till SP

--- source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
+++ source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NeoCortexApi.Encoders;
 using NeoCortexApi.Entities;
+using NeoCortexApi.Network;
+using NeoCortexApi.Utility;
 
 namespace NeoCortexApi.Experiments.SpatialPoolerInputReconstruction
 {
@@ -17,60 +19,66 @@ namespace NeoCortexApi.Experiments.SpatialPoolerInputReconstruction
         [TestCategory("Experiment")]
         public void Setup()
         {
-            int[] scalarInputs = { 1, 2, 3, 4, 5 };
-            // Find the maximum and minimum values
-            int maxValue = scalarInputs.Max();
-            int minValue = scalarInputs.Min();
-            InputParams inputs = new();
-            inputs.width=21;
-            inputs.maxIndex=5.0;
-            inputs.radius=-1.0;
-            inputs.compareNumber=1.0;
 
-            ProcessTestCase(scalarInputs, inputs, minValue, maxValue);
-        } 
-        
-        static void ProcessTestCase(int[] scalarInputs, InputParams inputs, double minValue, double maxValue)
-        {
-            ScalarEncoder encoder = new (new Dictionary<string, object>()
-            {
-                { "W", inputs.width},
-                { "N", 1024},
-                { "Radius", inputs.radius},
-                { "MinVal", minValue},
-                { "MaxVal", maxValue},
-                { "Periodic", false},
-                { "Name", "scalar"}, 
-                { "ClipInput", false},
-            });
-            List<int[]> sdrList = new ();
+            Console.WriteLine($"Hello NeocortexApi! Experiment {nameof(SpatialPoolerInputReconstructionExperiment)}");
+
+            double max = 5;
             
-            foreach (var i in scalarInputs)
+            // Used as a boosting parameters
+            // that ensure homeostatic plasticity effect.
+            double minOctOverlapCycles = 1.0;
+            double maxBoost = 5.0;
+
+            // We will use 200 bits to represent an input vector (pattern).
+            int inputBits = 200;
+
+            // We will build a slice of the cortex with the given number of mini-columns
+            int numColumns = 1024;
+
+            // This is a set of configuration parameters used in the experiment.
+            HtmConfig cfg = new HtmConfig(new int[] { inputBits }, new int[] { numColumns })
             {
-                var result = encoder.Encode(i);
-                sdrList.Add(result);
-                Console.WriteLine($"Input: {i} -> SDR: {string.Join("", result)}");
+                CellsPerColumn = 10,
+                MaxBoost = maxBoost,
+                DutyCyclePeriod = 100,
+                MinPctOverlapDutyCycles = minOctOverlapCycles,
+
+                GlobalInhibition = false,
+                NumActiveColumnsPerInhArea = 0.02 * numColumns,
+                PotentialRadius = (int)(0.15 * inputBits),
+                LocalAreaDensity = -1,
+                ActivationThreshold = 10,
+
+                MaxSynapsesPerSegment = (int)(0.01 * numColumns),
+                Random = new ThreadSafeRandom(42),
+                StimulusThreshold = 10,
+            };
+
+            // This dictionary defines a set of typical encoder parameters.
+            Dictionary<string, object> settings = new Dictionary<string, object>()
+            {
+                { "W", 21 },
+                { "N", inputBits },
+                { "Radius", -1.0 },
+                { "MinVal", 0.0 },
+                { "Periodic", false },
+                { "Name", "scalar" },
+                { "ClipInput", false },
+                { "MaxVal", max }
+            };
+            
+            EncoderBase encoder = new ScalarEncoder(settings);
+
+            // We create here 100 random input values.
+            List<double> inputValues = new List<double>();
+
+            for (int i = 0; i < (int)max; i++)
+            {
+                inputValues.Add((double)i);
             }
 
-            // Initialize the SpatialPooler and its connections
-            // SpatialPooler sp = new ();
-            // Connections connections = new ();
-            // sp.Init(connections);
-            //
-            // int[] sdr2 = sp.Compute(sdrList[0], false);
-            // Console.WriteLine($"Input: {1} -> SDR: {string.Join("", sdr2)}");
-
-            }
+            var sp = SpatialPoolerOutput(cfg, encoder, inputValues);
             
         }
-    public class InputParams
-    {
-        public int width { get; set; }
-        public double maxIndex { get; set; }
-        public double radius { get; set; }
-        public double compareNumber { get; set; }
-
     }
-
-
 }
