151b3393c9d4f2101ddd9ebbede757e5866045f0
Priyanshu Bandyopadhyay <priyanshu.banerjee1311@gmail.com>
2025-02-09 10:30:00 +0100

    code refinements

--- source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
+++ source/NeoCortexApi.Experiments/SpatialPoolerInputReconstructionExperiment.cs
@@ -622,9 +622,7 @@ namespace NeoCortexApi.Experiments
                 dir = dir.Parent;
             }
     
-            // Fallback if not found
-            Console.WriteLine("Warning: Project root not found. Using desktop instead.");
-            return Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            
         }
     }
 }
